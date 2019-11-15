3b7e104a0b28cc214d5f0591132e76d3880eade5
Abhi Mohnani <amohnan1@jhu.edu>
2019-11-15 15:29:16 -0500

    fixed Queen

--- Queen.cpp
+++ Queen.cpp
@@ -2,7 +2,7 @@
 
 using std::pair;
 
-bool Queen::legal_move_shape(pair<char,char> start, pair<char,char> end){
+bool Queen::legal_move_shape(pair<char,char> start, pair<char,char> end) const{
   return (start.first == end.first) || (start.second == end.second)
     || (start.first - end.first == start.second - end.second)
     || (start.first - end.first == -(start.second - end.second));
