7c6992b8037a9ffeb16e609889695af344888f0f
Abhi Mohnani <amohnan1@jhu.edu>
2019-11-18 13:52:27 -0500

    fixed pawn movement

--- Pawn.cpp
+++ Pawn.cpp
@@ -3,14 +3,31 @@
 using std::pair;
 
 bool Pawn::legal_move_shape(pair<char,char> start, pair<char,char> end) const{
-  if (start.second == 2){
-    return start.first == end.first && (end.second - start.second == 2);
+  if (start.first != end.first){
+    return false;
+  }
+  if (is_white()){
+    if (start.second == '2'){
+      return (end.second - start.second == 2) || end.second - start.second == 1;
+    }else{
+      return (end.second - start.second == 1);
+    }
   }else{
-    return start.first == end.first && (end.second - start.second == 1);
+    if (start.second == '7'){
+      return (start.second - end.second == 2) || start.second - end.second == 1;
+    }else{
+      return (start.second - end.second == 1);
+    }
   }
 }
 
 bool Pawn::legal_capture_shape(pair<char,char> start, pair<char,char> end) const{
-  return (end.second - start.second == 1) &&
-    (end.first - start.first == 1 || end.first - start.first == -1);
+  if  (!(end.first - start.first == 1 || end.first - start.first == -1)){
+    return false;
+  }
+  if (is_white()){
+    return start.second - end.second == 1;
+  }else{
+    return end.second - start.second == 1;
+  }
 }
